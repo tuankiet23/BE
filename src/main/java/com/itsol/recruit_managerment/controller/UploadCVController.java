3e6fe0e4da3c4f5a9732e0de188ab2b65b663eed
hoanggiap1234 <hoanggiap6984@gmail.com>
2021-12-26 11:06:00 +0700

    commit api ung tuyen

---
+++ src/main/java/com/itsol/recruit_managerment/controller/UploadCVController.java
@@ -0,0 +1,60 @@
+package com.itsol.recruit_managerment.controller;
+
+import com.itsol.recruit_managerment.model.FileInfo;
+import com.itsol.recruit_managerment.service.UploadCVService;
+import com.itsol.recruit_managerment.utils.ResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+//@RequestMapping("/api/user")
+public class UploadCVController {
+
+    @Autowired
+    private UploadCVService uploadCVService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file
+    , @RequestParam("username") String username ,@RequestParam("jobid") long jobId )
+    {
+        String message = "";
+        try {
+            uploadCVService.save(username, file, jobId);
+            message = "Uploaded cv and apply job successfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not upload and register job the file: " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<List<FileInfo>> getListFiles() {
+        List<FileInfo> fileInfos = uploadCVService.loadAll().map(path -> {
+            String filename = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder
+                    .fromMethodName(UploadCVController.class, "getFile", path.getFileName().toString()).build().toString();
+
+            return new FileInfo(filename, url);
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = uploadCVService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+}
